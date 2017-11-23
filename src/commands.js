f08fb98ecb4a7b205a160835b2a34d135e94a6c7
Hiroki Kumamoto <kumabook@live.jp>
2017-11-23 17:19:00 +0900

    Add commands.js that has command list

---
+++ src/commands.js
@@ -0,0 +1,12 @@
+const commands = [
+  'forward-char',
+  'backward-char',
+  'beginning-of-line',
+  'end-of-line',
+  'next-line',
+  'previous-line',
+  'end-of-buffer',
+  'beginning-of-buffer',
+];
+
+export default commands;
