082d442539fedeb162d7703d20b712435383b777
Hiroki Kumamoto <kumabook@live.jp>
2017-11-23 23:13:10 +0900

    Add Candidate component

---
+++ src/components/Candidate.jsx
@@ -0,0 +1,25 @@
+import browser from 'webextension-polyfill';
+import React     from 'react';
+import PropTypes from 'prop-types';
+
+function imageURL(type) {
+  return browser.extension.getURL(`images/${type}.png`);
+}
+
+const Candidate = ({ item, isSelected }) => (
+  <div className={isSelected ? 'candidate selected' : 'candidate'}>
+    <img className="candidate-icon" src={imageURL(item.type)} alt={item.type} />
+    <span>{item.id}</span>
+  </div>
+);
+
+Candidate.propTypes = {
+  item: PropTypes.shape({
+    id:   PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  isSelected: PropTypes.bool.isRequired,
+};
+
+export default Candidate;
