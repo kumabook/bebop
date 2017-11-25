4266b98721cd647ea50892eb9790ba042c649968
Hiroki Kumamoto <kumabook@live.jp>
2017-11-26 01:14:27 +0900

    Add .nycrc and setup.js

---
+++ test/setup.js
@@ -0,0 +1,27 @@
+const { JSDOM } = require('jsdom');
+const enzyme    = require('enzyme');
+const Adapter   = require('enzyme-adapter-react-16');
+const browser   = require('./browser_mock');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+global.browser = browser;
+global.chrome = null;
+
+copyProps(window, global);
+
+enzyme.configure({ adapter: new Adapter() });
