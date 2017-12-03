8325bca09d64fa803b80a3ad48a9e5ba34aa8fee
Hiroki Kumamoto <kumabook@live.jp>
2017-12-04 02:41:31 +0900

    Update test/key_sequences.test.js

--- test/key_sequences.test.js
+++ test/key_sequences.test.js
@@ -4,7 +4,7 @@ import keySequence from '../src/key_sequences';
 
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 function code(c) {
-  return c.charCodeAt(0);
+  return c.toUpperCase().charCodeAt(0);
 }
 
 function key(k, { s = false, c = false, m = false } = {}) {
@@ -17,8 +17,17 @@ function key(k, { s = false, c = false, m = false } = {}) {
   });
 }
 
+const up = 38;
+
 test('keySequence returns key sequences from keyEvent', (t) => {
+  const s = true;
+  const c = true;
+  const m = true;
   t.is(keySequence(key(code('a'))), 'a');
-  t.is(keySequence(key(code('a'), { c: true })), 'C-a');
-  t.is(keySequence(key(code('a'), { m: true })), 'M-a');
+  t.is(keySequence(key(code('a'), { s })), 'S-a');
+  t.is(keySequence(key(code('a'), { c })), 'C-a');
+  t.is(keySequence(key(code('a'), { m })), 'M-a');
+  t.is(keySequence(key(code('a'), { c, m })), 'C-M-a');
+  t.is(keySequence(key(up)), 'up');
+  t.is(keySequence(key(up, { s })), 'S-up');
 });
