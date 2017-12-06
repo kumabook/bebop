96dbb924a9340b26652d8daf2f7ad9d26a1751b7
Hiroki Kumamoto <kumabook@live.jp>
2017-12-06 10:24:03 +0900

    Remove warnings of index.js

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const webExt  = require('web-ext').default;
 const config  = require('./webpack.config');
 
 const compiler = webpack(config);
-const watching = compiler.watch({
-}, (err, stats) => {
-  console.log('weback built');
+const watching = compiler.watch({}, (err) => {
+  /* eslint-disable no-console */
+  if (err) {
+    console.log('\nFailed to webpack build');
+  } else {
+    console.log('\nweback built');
+  }
 });
 
 webExt.cmd.run({
   firefox:        process.env.FIREFOX_BINARY,
   sourceDir:      process.cwd(),
-  ignoreFiles:    ['**/*~', 'web-ext-artifacts/', 'src/', 'test/'],
+  ignoreFiles:    process.env.IGNORE_FILES.split(' ').concat('**/*~'),
   browserConsole: true,
 }, {
   shouldExitProgram: false,
-}).then((runner) => runner.registerCleanup(() => watching.close()));
+}).then(runner => runner.registerCleanup(() => watching.close()));
