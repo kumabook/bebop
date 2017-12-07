2f77a2aa88687a1930a7bcc387624a6fbd4d1e26
Hiroki Kumamoto <kumabook@live.jp>
2017-12-07 19:07:26 +0900

    Use `images/click.png` image if the role of a link candidate is `button`

--- src/sources/link.js
+++ src/sources/link.js
@@ -1,8 +1,16 @@
+import browser from 'webextension-polyfill';
 import { getFaviconUrl } from '../utils/url';
 import { sendMessageToActiveTab } from '../utils/tabs';
 import getMessage from '../utils/i18n';
 
 const linkMaxResults = 100;
+
+function faviconUrl(link) {
+  if (link.role === 'link') {
+    return getFaviconUrl(link.url);
+  }
+  return browser.extension.getURL('images/click.png');
+}
 
 export default function candidates(query, { maxResults = 20 } = {}) {
   return sendMessageToActiveTab({
@@ -11,14 +19,17 @@ export default function candidates(query, { maxResults = 20 } = {}) {
       query,
       maxResults: linkMaxResults,
     },
-  }).then(links => links.slice(0, maxResults).map(l => ({
-    id:         `content-link-${l.id}`,
-    label:      `${l.label}: ${l.url}`,
-    type:       'link',
-    name:       'open-link',
-    args:       [l],
-    faviconUrl: getFaviconUrl(l.url),
-  })))
+  }).then(links => links.slice(0, maxResults).map((l) => {
+    const { id } = l;
+    return {
+      id,
+      label:      `${l.label}: ${l.url}`,
+      type:       'link',
+      name:       'open-link',
+      args:       [l],
+      faviconUrl: faviconUrl(l),
+    };
+  }))
     .catch(() => [])
     .then(items => ({
       items,
