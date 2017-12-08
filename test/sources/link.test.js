e9ce3a33fbf731f9ef596c3ae4f2d5e00a249749
Hiroki Kumamoto <kumabook@live.jp>
2017-12-08 19:29:25 +0900

    Add link.candidates() test

--- test/sources/link.test.js
+++ test/sources/link.test.js
@@ -1,6 +1,28 @@
 import test from 'ava';
+import nisemono from 'nisemono';
 import { getFaviconUrl } from '../../src/utils/url';
-import { faviconUrl, getLabel } from '../../src/sources/link';
+import candidates, { faviconUrl, getLabel } from '../../src/sources/link';
+
+const { browser } = global;
+const { query, sendMessage } = browser.tabs;
+function setup() {
+  browser.tabs.query = nisemono.func();
+  nisemono.expects(browser.tabs.query).resolves([{
+    id:       'tab-0',
+    title:    'title',
+    url:      'https://example.com/',
+    windowId: 'window-0',
+  }]);
+  browser.tabs.sendMessage = nisemono.func();
+}
+
+function restore() {
+  browser.tabs.query = query;
+  browser.tabs.sendMessage = sendMessage;
+}
+
+test.beforeEach(setup);
+test.afterEach(restore);
 
 test('faviconUrl returns url', (t) => {
   const url = 'https://example.com';
@@ -15,4 +37,26 @@ test('getLabel returns label', (t) => {
   t.is(getLabel({ url, label }), `${label}: ${url}`);
   t.is(getLabel({ url, label: ' ' }), url);
   t.is(getLabel({ label }), label);
+});
+
+test.serial('candidates returns link candidates', (t) => {
+  nisemono.expects(browser.tabs.sendMessage).resolves([{
+    id:    'link-0',
+    label: 'title',
+    url:   'https://example.com/',
+    role:  'link',
+  }]);
+  return candidates('q').then(({ items, label }) => {
+    t.true(label !== null);
+    t.is(items.length, 1);
+    t.is(items[0].id, 'link-0');
+  });
+});
+
+test.serial('candidates returns link candidates', (t) => {
+  nisemono.expects(browser.tabs.sendMessage).rejects(new Error('error'));
+  return candidates('q').then(({ items, label }) => {
+    t.true(label !== null);
+    t.is(items.length, 0);
+  });
 });
