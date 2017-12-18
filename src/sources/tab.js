4a2e50e1ff2031480efb396c005493fdeadd715d
Hiroki Kumamoto <kumabook@live.jp>
2017-12-18 11:07:21 +0900

    Don't exclude active tab from tab candidates because close-tab command is implemented

--- src/sources/tab.js
+++ src/sources/tab.js
@@ -2,7 +2,7 @@ import browser from 'webextension-polyfill';
 import getMessage from '../utils/i18n';
 
 export default function candidates(q, { maxResults }) {
-  return browser.tabs.query({ active: false })
+  return browser.tabs.query({})
     .then((l) => {
       const items = l.filter(t => t.title.includes(q) || t.url.includes(q));
       return items.slice(0, maxResults).map(t => ({
