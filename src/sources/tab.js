52e2ee78e6bc8a7ec5f2133300b293aa546550ce
Hiroki Kumamoto <kumabook@live.jp>
2017-12-22 02:50:06 +0900

    Exclude `chrome-extension://` and `moz-extension://` url from tab candidates

--- src/sources/tab.js
+++ src/sources/tab.js
@@ -1,10 +1,16 @@
 import browser from 'webextension-polyfill';
 import getMessage from '../utils/i18n';
 
+function isCandidate(tab, q) {
+  const { title: t, url: u } = tab;
+  return (t.includes(q) || u.includes(q)) &&
+    !(u.startsWith('moz-extension') || u.startsWith('chrome-extension'));
+}
+
 export default function candidates(q, { maxResults }) {
   return browser.tabs.query({})
     .then((l) => {
-      const items = l.filter(t => t.title.includes(q) || t.url.includes(q));
+      const items = l.filter(t => isCandidate(t, q));
       return items.slice(0, maxResults).map(t => ({
         id:         `${t.id}`,
         label:      `${t.title}: ${t.url}`,
