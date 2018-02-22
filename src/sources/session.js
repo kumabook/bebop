d15b4f0ab253d55b422cbf4b3df48a89c6c961ed
Hiroki Kumamoto <kumabook@live.jp>
2018-02-23 00:50:08 +0900

    Narrows down session by label

--- src/sources/session.js
+++ src/sources/session.js
@@ -3,7 +3,9 @@ import getMessage from '../utils/i18n';
 
 export default function candidates(q, { maxResults } = {}) {
   return fetch(maxResults)
-    .then(items => items.map(session2candidate))
+    .then(items => items.map(session2candidate)
+                        .filter(item => item.label.includes(q))
+                        .slice(0, maxResults))
     .then(items => ({
       items,
       label: `${getMessage('sessions')} (:session or s)`,
